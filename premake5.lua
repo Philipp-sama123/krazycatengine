de4d658797f63682db1ee2d599180a035cab6422
Philipp <philipp.stampfer@protonmail.ch>
2026-09-18 00:33:34 +0200

    [INIT] create premake

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "KrazyCatEngine"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "KrazyCatEngine"
+    location "KrazyCatEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    include
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++22"
+        staticruntime "On"
+        systemversion "latest"
+
+    defines
+    {
+        "KCE_PLATFORM_WINDOWS",
+        "KCE_BUILD_DLL"
+    }
+
+    postbuildcommands
+    {
+        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+
+    filter "configurations:Debug"
+        defines "KCE_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "KCE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "KCE_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    include
+    {
+        "%{prj.name}/vendor/spdlog/include",
+        "KrazyCatEngine/src",
+    }
+
+    links
+    {
+        "KrazyCatEngine"
+    }
+
+    filter "system:windows"
+        cppdialect "C++22"
+        staticruntime "On"
+        systemversion "latest"
+
+    defines
+    {
+        "KCE_PLATFORM_WINDOWS",
+        "KCE_BUILD_DLL"
+    }
+
+    filter "configurations:Debug"
+        defines "KCE_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "KCE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "KCE_DIST"
+        optimize "On"
