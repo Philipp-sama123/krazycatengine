d3aa65d31cd58ed85948093797fef3a2d6e26100
Philipp <philipp.stampfer@protonmail.ch>
2026-09-17 15:37:56 +0200

    [INIT] -missing files

---
+++ krazycatengine/Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,17 @@
+﻿#include <KrazyCatEngine.h>
+
+class Sandbox : public KrazyCatEngine::Application
+{
+public: 
+    Sandbox() {
+
+    }
+
+    ~Sandbox() {
+        
+    }
+};
+KrazyCatEngine::Application* KrazyCatEngine::CreateApplication()
+{
+    return new Sandbox();
+}
